7610d1a8382a7231b702c878ef87ce062268556b
Guido La Rosa <guido.glarosa@gmail.com>
2017-03-24 20:38:06 -0300

    About.

--- public/js/index.js
+++ public/js/index.js
@@ -9,6 +9,7 @@ const notFound = require('./views/base/notfound.pug');
 
 // Views
 const home = require('./views/home/index');
+const about = require('./views/about/index');
 
 // Vendor global scripts
 const pace = require('pace-progress');
@@ -63,6 +64,8 @@ $(document).ready(() => {
   });
 
   page('/', home);
+
+  page('/about', about);
 
   // 404 handler
   // Executes when no other route was found
